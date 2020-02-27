189039d90e35c81983aca03f6a678ff0fcae65c3
grwest <57156650+grwest@users.noreply.github.com>
2020-02-26 16:48:00 -0800

    Add files via upload

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: grwest <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 16:42:29 by grwest            #+#    #+#             */
-/*   Updated: 2020/02/26 16:19:08 by grwest           ###   ########.fr       */
+/*   Updated: 2020/02/26 16:46:59 by grwest           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_bzero(void *s, size_t n)
 int		main(void)
 {
 	char	str[50];
-	
+
 	ft_strcpy(str, "This is a string. This is a string.");
 	ft_bzero(str, 15);
 	ft_putstr(str);
